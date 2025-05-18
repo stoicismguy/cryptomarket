af68a23a434b3388df1b0a2002a6d4d24eaeae9c
stoicismguy <pyzan55@gmail.com>
2025-05-18 17:53:31 +0500

    feat: fixes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,14 +3,14 @@ set -e  # Выход при ошибке
 source .env
 poetry install
 
-poetry run python src/manage.py migrate
+poetry run python cryptomarket/manage.py migrate
 
 echo "SUKA PYTHON"
 
 echo $DEBUG
 if [ "$DEBUG" == 1 ]; then
     echo "Запуск сервера в режиме разработки"
-    exec poetry run python src/manage.py runserver 0.0.0.0:8000
+    exec poetry run python cryptomarket/manage.py runserver 0.0.0.0:8000
 else
     echo "Запуск сервера в режиме продакшена"
     exec poetry run gunicorn --bind 0.0.0.0:8000 cryptomarket.wsgi:application
