1ef475333c6d259ba7d77adebc53747716f18706
stoicismguy <pyzan55@gmail.com>
2025-05-18 20:11:18 +0500

    feat: new migr

--- cryptomarket/users/models.py
+++ cryptomarket/users/models.py
@@ -26,7 +26,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=255, unique=True)
     role = models.CharField(max_length=20, choices=UserRole.choices, default=UserRole.USER)
-    api_key = models.UUIDField(default=uuid4, unique=True, editable=False)
+    api_key = models.UUIDField(default=uuid4, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
